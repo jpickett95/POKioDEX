52abdabf41d0e5db799110bc75eaab8ebd7dfa76
Jonah Pickett <jpickettpre@gmail.com>
2023-03-20 13:09:17 -0400

    Added folders, pokemon.json file, pokemonModel & pokemonViewModel; also created PokedexCellView

---
+++ Pokiodex_io/Pokiodex_io/Models/PokemonModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonModel.swift
+//  Pokiodex_io
+//
+//  Created by Jonah Pickett on 3/20/23.
+//
+
+import Foundation
---
+++ Pokiodex_io/Pokiodex_io/ViewModels/PokemonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonViewModel.swift
+//  Pokiodex_io
+//
+//  Created by Jonah Pickett on 3/20/23.
+//
+
+import Foundation
